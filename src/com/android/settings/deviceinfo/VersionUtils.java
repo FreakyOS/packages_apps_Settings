1ed4c092a3be79eebac024665a557078495116fd
bunnyyTheFreak <hsinghalk@yahoo.com>
2020-12-05 15:32:31 +0000

    Settings: fix Version class declaration
    Change-Id: Ib514efb07004becc90f356ccb3e159f9830b61fc
    Signed-off-by: bunnyyTheFreak <hsinghalk@yahoo.com>

--- src/com/android/settings/deviceinfo/VersionUtils.java
+++ src/com/android/settings/deviceinfo/VersionUtils.java
@@ -4,7 +4,7 @@ package com.android.settings.deviceinfo;
 import android.os.SystemProperties;
 
 public class VersionUtils {
-    public static String getPixelExperienceVersion(){
+    public static String getFreakyVersion(){
         return SystemProperties.get("org.freaky.version.display","");
     }
 }
